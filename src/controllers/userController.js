faffd432247b5f6d35d40b9ef9c1c1f542d00283
Ahmed Arian <ahmed.arian@fera.co.uk>
2025-02-22 22:35:42 +0000

    minor corrections

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,8 +3,9 @@ import ResellerRequest from '../models/resellerRequestModel.js';
 
 export const applyReseller = async (request, h) => {
   try {
-    const user_id = request.auth.credentials.id;
-    const { id_document } = request.payload;
+    const user_id = request.auth.credentials.userId;
+
+    const { firstname, lastname, dob, gender, billing_added, id_type, id_document, mobile_number } = request.payload;
 
     const requestExists = await ResellerRequest.findOne({ where: { user_id, status: 'pending' } });
 
@@ -12,7 +13,21 @@ export const applyReseller = async (request, h) => {
       return h.response({ message: 'Reseller request already pending' }).code(400);
     }
 
-    await ResellerRequest.create({ user_id, id_document });
+    if (firstname && billing_added && id_document) {
+      await ResellerRequest.create({ user_id, id_document });
+    } else {
+      return h.response({ message: 'Invalid, Provide required details and attach an ID document.' }).code(400);
+    }
+
+    const existingUser = await User.findOne({ where: { user_id } });
+    const existingUserProfile = await UserProfile.findOne({ where: { user_id } });
+    
+    if (existingUser && existingUserProfile) {
+      existingUser.update({ firstname: firstname, lastname: lastname });
+      existingUserProfile.update({ dob: dob, gender: gender, billing_address: billing_added, mobile_number: mobile_number });
+    } else {
+      return h.response({ message: 'User has not registered.' }).code(422);
+    }
 
     return h.response({ message: 'Reseller request submitted' }).code(201);
   } catch (error) {
--- src/models/userProfile.js
+++ src/models/userProfile.js
@@ -4,8 +4,10 @@ import sequelize from '../config/database.js';
 const UserProfile = sequelize.define('UserProfile', {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     user_id: { type: DataTypes.INTEGER, allowNull: false, references: { model: 'Users', key: 'id' } },
-    mobile_number: { type: DataTypes.STRING, allowNull: true },
+    dob: { type: DataTypes.STRING, allowNull: true },
+    gender: { type: DataTypes.STRING, allowNull: true },
     billing_address: { type: DataTypes.TEXT, allowNull: true },
+    mobile_number: { type: DataTypes.STRING, allowNull: true },
 }, { timestamps: true });
 
 export default UserProfile;
